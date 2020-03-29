c0d78e9bf53ab3baf94391a27fd036186eae805f
Vincent Kocupyr <vincent.kocupyr@gmail.com>
2020-03-29 16:41:27 +0200

    feat(navigation): add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 
-import Home from './screens/Home';
+import HomeScreen from './screens/Home';
+import DetailsScreen from './screens/Details';
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Details" screenOptions={screenOptions}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+const Stack = createStackNavigator();
+
+const screenOptions = {
+  ...TransitionPresets.SlideFromRightIOS,
+};
